a2261afd4f1a64b84f3edd6973d9143fd6ee62fa
Jessica Ledbetter <jessica@jessicaledbetter.com>
2011-02-28 12:43:01 -0800

    Baseline version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leido::Application.config.secret_token = 'c7384ba8b08192b8b2681612806e6f923a580be378471a5230fd0c9c53deda97fd3d1d1e7d0696f343496a05cb23bc01bea1902e72ac68a795dcb96cea354678'
