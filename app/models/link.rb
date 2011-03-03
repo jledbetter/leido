83f827bdf37de2a01232d8a84d2e4c14cceb500b
Jessica Ledbetter <jessica@jessicaledbetter.com>
2011-03-03 10:28:17 -0800

    Made url be unique

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,5 +2,7 @@ class Link < ActiveRecord::Base
   validates :url, :presence => true
   validates :title, :presence => true
   
+  validates_uniqueness_of :url, :case_sensitive => false
+  
   belongs_to :user
 end
